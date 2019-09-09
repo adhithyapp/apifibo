4f2a286c0897d9900b72af646863a4a09cb51950
adhithyapp <51515095+adhithyapp@users.noreply.github.com>
2019-09-09 05:26:16 -0700

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("Even Numbers :");
+            for (i = 1; i <= 30; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            int i = 0;
+            Console.WriteLine("\nOdd Numbers :");
+            for (i = 1; i <= 30; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            int f = 1;
+            int s = 1;
+            int r = 0;
+            string series = "";
+            for (int i = 0; i < 10; i++)
+            {
+                series = series + "" + s.ToString();
+                f = s;
+                s = r;
+                r = s + f;
+            }
+
+        }
+    }
+}
